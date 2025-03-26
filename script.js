2b9f018be8bcc8ccd850cf796d51197f1b852168
DevDiscere <dev.ginomiguel@gmail.com>
2025-03-26 21:59:54 +0800

    Add getComputerChoice function and logic
    I added a getComputerChoice function to generate a random number from 1
    to 3. If the value is 1, the computerChoice is "Rock". If the value is
    2, the computerChoice is "Paper". If the value is 3, the computerChoice
    is "Scissors".

--- script.js
+++ script.js
@@ -1,1 +1,14 @@
-console.log("Hello World");
+function getComputerChoice () {
+    let randomNumber = 0;
+    let computerChoice = "";
+    
+    randomNumber = Math.floor(Math.random() * 3) + 1;
+    switch(randomNumber) {
+        case 1:
+            return computerChoice = "Rock";
+        case 2:
+            return computerChoice = "Paper";
+        case 3:
+            return computerChoice = "Scissors";
+    }
+}
