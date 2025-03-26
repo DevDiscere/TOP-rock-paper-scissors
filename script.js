5439c81560d2699398902b8ec762f3cb2c041fd9
DevDiscere <dev.ginomiguel@gmail.com>
2025-03-26 23:17:35 +0800

    Add playRound function and logic
    I added a playRound function to simulate the logic of Rock Paper
    Scissors. Before comparing the user's and computer's hands, I lower-case
    the inputs to make the string comparison easier. After that, I
    formatted their strings by finding the first letter, making it uppercase
    and then slicing the string after the first letter, and finally
    concatenating it with the uppercased first letter.
    
    For the main logic of the game, I first compared if the two hands are
    equal to display a tie. Another condition compares if the user's hand
    wins against the computer, if so, increment the user's score and display
    user's win. If not, condition compares if the computer's hand wins
    against the user, if so, increment the computer's score and display
    user's lose.

--- script.js
+++ script.js
@@ -19,3 +19,40 @@ function getHumanChoice() {
     humanChoice = prompt('Play a hand ("Rock", "Paper", "Scissors"): ');
     return humanChoice;
 }
+
+let humanScore = 0, computerScore = 0;
+
+function playRound(humanChoice, computerChoice) {
+    humanChoice = humanChoice.toLowerCase();
+    computerChoice = computerChoice.toLowerCase();
+
+    let formattedHumanChoice = humanChoice.charAt(0).toUpperCase().concat(humanChoice.slice(1));
+    let formattedComputerChoice = computerChoice.charAt(0).toUpperCase().concat(computerChoice.slice(1));
+    let result = "";
+
+    if (humanChoice == computerChoice) {
+        return result = `It's a tie! ${formattedHumanChoice} does not beat ${formattedComputerChoice}.`;
+    }
+
+    if (
+        (humanChoice == "rock" && computerChoice == "scissors") 
+        || (humanChoice == "paper" && computerChoice == "rock") 
+        || (humanChoice == "scissors" && computerChoice == "paper")
+    ) {
+        ++humanScore;
+        return result = `You win! ${formattedHumanChoice} beats ${formattedComputerChoice}.`;
+    }
+    else if (
+        (computerChoice == "rock" && humanChoice == "scissors") 
+        || (computerChoice == "paper" && humanChoice == "rock") 
+        || (computerChoice == "scissors" && humanChoice == "paper")
+    ) {
+        ++computerScore;
+        return result = `You lose! ${formattedComputerChoice} beats ${formattedHumanChoice}.`;
+    }
+}
+
+const computerSelection = getComputerChoice();
+const humanSelection = getHumanChoice();
+
+console.log(playRound(humanSelection, computerSelection));
