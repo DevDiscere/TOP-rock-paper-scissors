a87b7870bebf094cf165a4400220485baab2ba24
DevDiscere <dev.ginomiguel@gmail.com>
2025-04-01 14:58:05 +0800

    Add images and CSS, modify HTML and JavaScript
    I added images to serve as illustrations for the hand choices in the
    game. I also added a stylesheet to prepare the website for design and
    provide hide functionality.
    
    I modified the HTML page to restructure its content and add initial
    classes. This is then utilized by the JS script.
    
    In the script, I built the logic of the game from the ground-up while
    using the button's addEventlistener as input. I also utilized DOM
    manipulation to provide functionality to the HTML content of the game.
    
    It took a lot of trials and error but I managed to make it work. Go me!

--- script.js
+++ script.js
@@ -1,90 +1,145 @@
-function getComputerChoice() {
-    const NUMBER_OF_HANDS = 3;
-    const OFFSET_BY_ONE = 1;
+function getHumanChoice (buttonEvent) {
+    let targetButtonClass = buttonEvent.target.classList.value;
     
+    switch (targetButtonClass) {
+        case "rock-button": return "Rock";
+        case "paper-button": return "Paper";
+        case "scissors-button": return "Scissors";
+    }
+}
+
+function getComputerChoice () {
+    const NUMBER_OF_HANDS = 3, OFFSET_BY_ONE = 1;
     let randomNumber = Math.floor(Math.random() * NUMBER_OF_HANDS) + OFFSET_BY_ONE;
-    switch(randomNumber) {
+
+    switch (randomNumber) {
         case 1: return "Rock";
         case 2: return "Paper";
         case 3: return "Scissors";
     }
+};
+
+function displayHandChoice (humanChoice, computerChoice) {
+    function getImageSource (handChoice) {
+        switch (handChoice) {
+            case "Rock": return "./images/rock_hand.png";
+            case "Paper": return "./images/paper_hand.png";
+            case "Scissors": return "./images/scissors_hand.png";
+        }
+    }
+
+    humanHandImage.setAttribute("src", getImageSource(humanChoice));
+    computerHandImage.setAttribute("src", getImageSource(computerChoice));
 }
 
-function playerSelection(event) {
-    let targetButton = event.target.classList.value;
-    let humanChoice = "";
+function playRound (humanChoice, computerChoice) {
+    let humanDecision = humanChoice.toLowerCase();
+    let computerDecision = computerChoice.toLowerCase();
 
-    switch(targetButton) {
-        case "rock-button":
-            humanChoice = "Rock";
-            console.log(`You played ${humanChoice}!`);
-            // Play Round
-            break;
-        case "paper-button":
-            humanChoice = "Paper";
-            console.log(`You played ${humanChoice}!`);
-            // Play Round
-            break;
-        case "scissors-button":
-            humanChoice = "Scissors";
-            console.log(`You played ${humanChoice}!`);
-            // Play Round
-            break;
+    if (humanDecision == computerDecision) {
+        resultContainer.textContent = `It's a tie! ${humanChoice} does not beat ${computerChoice}.`;
+    } else if (
+        (humanDecision == "rock" && computerDecision == "scissors") 
+        || (humanDecision == "paper" && computerDecision == "rock") 
+        || (humanDecision == "scissors" && computerDecision == "paper")
+    ) {
+        ++humanScore;
+        resultContainer.textContent = `You win! ${humanChoice} beats ${computerChoice}.`;
+    } else if (
+        (computerDecision == "rock" && humanDecision == "scissors") 
+        || (computerDecision == "paper" && humanDecision == "rock") 
+        || (computerDecision == "scissors" && humanDecision == "paper")
+    ) {
+        ++computerScore;
+        resultContainer.textContent = `You lose! ${computerChoice} beats ${humanChoice}.`;
     }
+    
+    humanScoreParagraph.textContent = `You: ${humanScore}`;
+    computerScoreParagraph.textContent = `Computer: ${computerScore}`;
 }
 
-function playGame(numberOfTries) {
-    let humanScore = 0, computerScore = 0;
+function playGame (buttonEvent) {
+    let humanChoice = getHumanChoice(buttonEvent);
+    let computerChoice = getComputerChoice();
 
-    function playRound(humanChoice, computerChoice) {
-        humanChoice = humanChoice.toLowerCase();
-        computerChoice = computerChoice.toLowerCase();
+    displayHandChoice(humanChoice, computerChoice);
+    playRound(humanChoice, computerChoice);
+}
 
-        let formattedHumanChoice = humanChoice.charAt(0).toUpperCase().concat(humanChoice.slice(1));
-        let formattedComputerChoice = computerChoice.charAt(0).toUpperCase().concat(computerChoice.slice(1));
-        let result = "";
+function displayGameWinner (humanScore, computerScore) {
+    winnerContainer.classList.remove("hide");
+    commandContainer.classList.remove("hide");
 
-        if (humanChoice == computerChoice) {
-            return result = `It's a tie! ${formattedHumanChoice} does not beat ${formattedComputerChoice}.`;
-        }
-
-        if (
-            (humanChoice == "rock" && computerChoice == "scissors") 
-            || (humanChoice == "paper" && computerChoice == "rock") 
-            || (humanChoice == "scissors" && computerChoice == "paper")
-        ) {
-            ++humanScore;
-            return result = `You win! ${formattedHumanChoice} beats ${formattedComputerChoice}.`;
-        }
-        else if (
-            (computerChoice == "rock" && humanChoice == "scissors") 
-            || (computerChoice == "paper" && humanChoice == "rock") 
-            || (computerChoice == "scissors" && humanChoice == "paper")
-        ) {
-            ++computerScore;
-            return result = `You lose! ${formattedComputerChoice} beats ${formattedHumanChoice}.`;
-        }
-    }
-
-    let humanSelection = "", computerSelection = "";
-
-    for(i = 0; i < numberOfTries; i++) {
-        humanSelection = getHumanChoice();
-        computerSelection = getComputerChoice();
-        console.log(playRound(humanSelection, computerSelection));
-    }
-
-    console.log(`You: ${humanScore}, Computer: ${computerScore}`);
-
-    if (humanScore > computerScore) {
-        console.log("YOU WIN!!!");
+    if (humanScore == computerScore) {
+        winnerContainer.textContent = "NO ONE WINS...";
+    } else if (humanScore > computerScore) {
+        winnerContainer.textContent = "YOU WIN! CONGRATS.";
     } else if (humanScore < computerScore) {
-        console.log("COMPUTER WINS!!!")
-    } else {
-        console.log("NO ONE WINS...")
+        winnerContainer.textContent = "YOU LOSE! GOODLUCK NEXT TIME.";
     }
 }
 
-const body = document.querySelector("body");
-body.addEventListener('click', playerSelection);
-// playGame(5);
+const startButton = document.querySelector(".start-button");
+const section = document.querySelector("section");
+startButton.addEventListener("click", (event) => {
+    event.target.classList.add("hide");
+    section.classList.remove("hide");
+});
+
+const NUMBER_OF_ROUNDS = 5;
+let gameRound = 1, humanScore = 0, computerScore = 0;
+let humanHandImage = document.querySelector(".human-hand");
+let computerHandImage = document.querySelector(".computer-hand");
+let winnerContainer = document.querySelector(".winner-container");
+let resultContainer = document.querySelector(".result-container");
+let humanScoreParagraph = document.querySelector(".human-score");
+let computerScoreParagraph = document.querySelector(".computer-score");
+let commandContainer = document.querySelector(".command-container");
+
+const choiceButtons = document.querySelectorAll(".choice-container button");
+choiceButtons.forEach(button => {
+    button.addEventListener("click", (event) => {
+        if (gameRound == NUMBER_OF_ROUNDS) {
+            disableChoiceButtons();
+            playGame(event);
+            displayGameWinner(humanScore, computerScore);
+        } else if (gameRound < NUMBER_OF_ROUNDS) {
+            playGame(event);
+            ++gameRound;
+        }
+    });
+});
+
+function resetGame () {
+    gameRound = 1, humanScore = 0, computerScore = 0;
+
+    humanHandImage.setAttribute("src", "./images/default_choice.png");
+    computerHandImage.setAttribute("src", "./images/default_choice.png");
+
+    enableChoiceButtons();
+
+    winnerContainer.classList.add("hide");
+
+    humanScoreParagraph.textContent = `You: ${humanScore}`;
+    computerScoreParagraph.textContent = `Computer: ${computerScore}`;
+    resultContainer.textContent = "ROCK. PAPER. SCISSORS.";
+
+    commandContainer.classList.add("hide");
+}
+
+const resetButton = document.querySelector(".reset-button");
+resetButton.addEventListener("click", resetGame);
+
+const quitButton = document.querySelector(".quit-button");
+quitButton.addEventListener("click", () => {
+    resetGame();
+    section.classList.add("hide");
+    startButton.classList.remove("hide");
+})
+
+function enableChoiceButtons () {
+    choiceButtons.forEach(button => button.removeAttribute("disabled"));
+}
+function disableChoiceButtons () {
+    choiceButtons.forEach(button => button.setAttribute("disabled", "disabled"));
+}
