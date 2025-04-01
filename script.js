85d66d48e0b6d45a32c1d69c4aeeb0b1e2707cc1
DevDiscere <dev.ginomiguel@gmail.com>
2025-04-01 23:43:39 +0800

    Add CSS styles, replace images, modify HTML and JS
    I added custom CSS styles to design my website. I also used Josh W.
    Comeau's CSS reset to remove the browser's initial CSS styles.
    
    I replaced the png images with svg images so that I can resize them
    without losing quality.
    
    I modified the some of the HTML structure to better contain what I
    envision the web project would look like. I also modified some of the JS
    script behavior to better align with my vision.

--- script.js
+++ script.js
@@ -22,9 +22,9 @@ function getComputerChoice () {
 function displayHandChoice (humanChoice, computerChoice) {
     function getImageSource (handChoice) {
         switch (handChoice) {
-            case "Rock": return "./images/rock_hand.png";
-            case "Paper": return "./images/paper_hand.png";
-            case "Scissors": return "./images/scissors_hand.png";
+            case "Rock": return "./images/rock_hand.svg";
+            case "Paper": return "./images/paper_hand.svg";
+            case "Scissors": return "./images/scissors_hand.svg";
         }
     }
 
@@ -37,25 +37,25 @@ function playRound (humanChoice, computerChoice) {
     let computerDecision = computerChoice.toLowerCase();
 
     if (humanDecision == computerDecision) {
-        resultContainer.textContent = `It's a tie! ${humanChoice} does not beat ${computerChoice}.`;
+        resultContainer.textContent = `Round ${gameRound}: ${humanChoice} does not beat ${computerChoice}.`;
     } else if (
         (humanDecision == "rock" && computerDecision == "scissors") 
         || (humanDecision == "paper" && computerDecision == "rock") 
         || (humanDecision == "scissors" && computerDecision == "paper")
     ) {
         ++humanScore;
-        resultContainer.textContent = `You win! ${humanChoice} beats ${computerChoice}.`;
+        resultContainer.textContent = `Round ${gameRound}: ${humanChoice} beats ${computerChoice}.`;
     } else if (
         (computerDecision == "rock" && humanDecision == "scissors") 
         || (computerDecision == "paper" && humanDecision == "rock") 
         || (computerDecision == "scissors" && humanDecision == "paper")
     ) {
         ++computerScore;
-        resultContainer.textContent = `You lose! ${computerChoice} beats ${humanChoice}.`;
+        resultContainer.textContent = `Round ${gameRound}: ${computerChoice} beats ${humanChoice}.`;
     }
     
-    humanScoreParagraph.textContent = `You: ${humanScore}`;
-    computerScoreParagraph.textContent = `Computer: ${computerScore}`;
+    humanScoreParagraph.textContent = `YOU: ${humanScore}`;
+    computerScoreParagraph.textContent = `COMPUTER: ${computerScore}`;
 }
 
 function playGame (buttonEvent) {
@@ -69,25 +69,28 @@ function playGame (buttonEvent) {
 function displayGameWinner (humanScore, computerScore) {
     winnerContainer.classList.remove("hide");
     commandContainer.classList.remove("hide");
+    choiceContainer.classList.add("hide");
 
     if (humanScore == computerScore) {
-        winnerContainer.textContent = "NO ONE WINS...";
+        winnerContainer.textContent = "IT'S A TIE! NO ONE WINS.";
     } else if (humanScore > computerScore) {
-        winnerContainer.textContent = "YOU WIN! CONGRATS.";
+        winnerContainer.textContent = "YOU WIN! CONGRATULATIONS.";
     } else if (humanScore < computerScore) {
         winnerContainer.textContent = "YOU LOSE! GOODLUCK NEXT TIME.";
     }
 }
 
 const startButton = document.querySelector(".start-button");
-const section = document.querySelector("section");
+const gameContainer = document.querySelector(".game-container");
 startButton.addEventListener("click", (event) => {
     event.target.classList.add("hide");
-    section.classList.remove("hide");
+    imageContainer.classList.add("hide");
+    gameContainer.classList.remove("hide");
 });
 
 const NUMBER_OF_ROUNDS = 5;
 let gameRound = 1, humanScore = 0, computerScore = 0;
+let imageContainer = document.querySelector(".image-container");
 let humanHandImage = document.querySelector(".human-hand");
 let computerHandImage = document.querySelector(".computer-hand");
 let winnerContainer = document.querySelector(".winner-container");
@@ -95,12 +98,12 @@ let resultContainer = document.querySelector(".result-container");
 let humanScoreParagraph = document.querySelector(".human-score");
 let computerScoreParagraph = document.querySelector(".computer-score");
 let commandContainer = document.querySelector(".command-container");
+let choiceContainer = document.querySelector(".choice-container");
 
 const choiceButtons = document.querySelectorAll(".choice-container button");
 choiceButtons.forEach(button => {
     button.addEventListener("click", (event) => {
         if (gameRound == NUMBER_OF_ROUNDS) {
-            disableChoiceButtons();
             playGame(event);
             displayGameWinner(humanScore, computerScore);
         } else if (gameRound < NUMBER_OF_ROUNDS) {
@@ -113,18 +116,17 @@ choiceButtons.forEach(button => {
 function resetGame () {
     gameRound = 1, humanScore = 0, computerScore = 0;
 
-    humanHandImage.setAttribute("src", "./images/default_choice.png");
-    computerHandImage.setAttribute("src", "./images/default_choice.png");
-
-    enableChoiceButtons();
-
-    winnerContainer.classList.add("hide");
+    humanHandImage.setAttribute("src", "./images/default_choice.svg");
+    computerHandImage.setAttribute("src", "./images/default_choice.svg");
 
     humanScoreParagraph.textContent = `You: ${humanScore}`;
     computerScoreParagraph.textContent = `Computer: ${computerScore}`;
-    resultContainer.textContent = "ROCK. PAPER. SCISSORS.";
+    resultContainer.textContent = "Choose a hand to throw.";
 
+    winnerContainer.classList.add("hide");
     commandContainer.classList.add("hide");
+
+    choiceContainer.classList.remove("hide");
 }
 
 const resetButton = document.querySelector(".reset-button");
@@ -133,13 +135,7 @@ resetButton.addEventListener("click", resetGame);
 const quitButton = document.querySelector(".quit-button");
 quitButton.addEventListener("click", () => {
     resetGame();
-    section.classList.add("hide");
+    gameContainer.classList.add("hide");
+    imageContainer.classList.remove("hide");
     startButton.classList.remove("hide");
 })
-
-function enableChoiceButtons () {
-    choiceButtons.forEach(button => button.removeAttribute("disabled"));
-}
-function disableChoiceButtons () {
-    choiceButtons.forEach(button => button.setAttribute("disabled", "disabled"));
-}
